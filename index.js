30df70fb4f3ee27c95f7018f389d92bea476fbea
Marius Alban <malban298@gmail.com>
2023-07-24 12:38:46 -0300

    lab complete

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 // Write your solution here!
+
 const cats = ["Milo", "Otis", "Garfield"];
-function destructivelyAppendCat(name) {
-  return cats.push(name);
+
+function destructivelyAppendCat() {
+  return cats.push("Ralph");
 }
-function destructivelyPrependCat(name) {
-  return cats.unshift(name);
+
+function destructivelyPrependCat() {
+  return cats.unshift("Bob");
 }
+
 function destructivelyRemoveLastCat() {
   return cats.pop();
 }
+
 function destructivelyRemoveFirstCat() {
   return cats.shift();
 }
+
 function appendCat(name) {
   const newCats = [...cats, name];
   return newCats;
@@ -20,10 +26,12 @@ function prependCat(name) {
   const newCats = [name, ...cats];
   return newCats;
 }
+
 function removeLastCat() {
-  const newCats = cats.slice(0, 2);
+  const newCats = cats.slice(0, cats.length - 1);
   return newCats;
 }
+
 function removeFirstCat() {
   const newCats = cats.slice(1);
   return newCats;
